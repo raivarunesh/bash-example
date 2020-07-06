bf32cd5eebd443730e2517307ff7e53a9e00b253
raivarunesh <rai.varunesh@gmail.com>
2020-07-07 09:52:56 +1000

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,7 +17,6 @@ echo -e "\033[33mCongratulations!\033[0m You've successfully run your first buil
 
 \033[33m$(cat artifacts/thumbsup.txt)\033[0m
 
-If you have any questions or need help email support@buildkite.com, we'd be happy to help!
 
 \033[31m<3\033[0m Buildkite
 "
